ad7117818ca2a45ccac45cfb51b68354b126d62c
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-04-21 23:28:41 +0600

    Time: 1244 ms (11.27%) | Memory: 18.3 MB (90.55%) - LeetSync

--- LinkedList/214-shortest-palindrome/shortest-palindrome.py
+++ LinkedList/214-shortest-palindrome/shortest-palindrome.py
@@ -1,19 +1,5 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        suffix,prefix = 0,0
-        base,power = 29,1
-        last_ind = 1
-        mod = 10**9+7
-        for i,c in enumerate(s):
-            char = ord(c)-ord('a') + 1
-            prefix = (prefix * base) % mod
-            prefix = (prefix + char) % mod
-            
-            suffix = (suffix + char* power) % mod
-            power = (power * base) % mod
-
-            if prefix == suffix:last_ind = i + 1
-  
-        suffix = s[last_ind:]
-        
-        return suffix[::-1] + s
+        for i in range(len(s), -1, -1):
+            if s[:i] == s[:i][::-1]:
+                return s[i:][::-1] + s
