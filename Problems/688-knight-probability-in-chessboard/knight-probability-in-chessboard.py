f427e718241f8cbb56ee1eeb77cead0bcd85408e
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-04-25 11:57:18 +0600

    Time: 87 ms (91.52%) | Memory: 26.8 MB (26.97%) - LeetSync

--- Problems/688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ Problems/688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -1,24 +1,26 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        if k == 0:
+            return 1
+        
+        @cache
+        def dp(i, j, ml):
 
-        dp = [[[0] * (k + 1) for _ in range(n)] for _ in range(n)]
-        moves = [(-2,1),(-2,-1),(2,1),(2,-1),(1,2),(1,-2),(-1,-2),(-1,2)]
+            if i < 0 or i>= n or j < 0 or j >= n:
+                return 0
+            if ml == 0:
+                return 1
+            
+            return (
+                dp(i+1, j+2, ml - 1) +
+                dp(i-1, j+2, ml - 1) +
+                dp(i+2, j+1, ml - 1) +
+                dp(i+2, j-1, ml - 1) +
+                dp(i+1, j-2, ml - 1) +
+                dp(i-1, j-2, ml - 1) +
+                dp(i-2, j+1, ml - 1) +
+                dp(i-2, j-1, ml - 1)  
 
-        dp[row][column][0] = 1
+            )/8
 
-        for step in range(1,k+1):
-            for i in range(n):
-                for j in range(n):
-                   
-                    for x,y  in moves:
-                        x += i
-                        y += j
-                        
-                        if 0<=x<n and 0<=y<n:
-                            dp[i][j][step] += 0.125 * dp[x][y][step - 1]
-                           
-        ans = 0
-        for i in range(n):
-            for j in range(n):
-                ans += dp[i][j][k]        
-        return ans
+        return dp(row, column, k) 
