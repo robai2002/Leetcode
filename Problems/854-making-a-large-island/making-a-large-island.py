f889c43639b6f30578b1f6a775d9995435f0e2e3
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-04-25 16:02:11 +0600

    Time: 1041 ms (48.15%) | Memory: 28.6 MB (62.35%) - LeetSync

---
+++ Problems/854-making-a-large-island/making-a-large-island.py
@@ -0,0 +1,34 @@
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        ans = [0,0]
+        def dfs(r: int,c: int,val: int) -> int:
+            if r >= len(grid) or r<0 or c>=len(grid) or c<0 or grid[r][c] == 0 or grid[r][c] == val:
+                return 0
+            grid[r][c] = val
+            return  dfs(r+1,c,val) +dfs(r-1,c,val) + dfs(r,c+1,val) + dfs(r,c-1,val)+1
+        def get_ans(i: int,j: int) -> int:
+            nig = [(0,-1),(0,1),(1,0),(-1,0)]
+            s = set()
+            for x,y in nig:
+                x += i
+                y += j
+                if -1<x<len(grid) and -1<y<len(grid):
+                    s.add(grid[x][y])
+                p = 1
+            for v in s:
+                p += ans[v]
+            return p
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                   x = dfs(i,j,len(ans))
+                   ans.append(x)
+        result = max(ans)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if not grid[i][j]:
+                    result = max(result,get_ans(i,j))
+        return result
+                    
+        
