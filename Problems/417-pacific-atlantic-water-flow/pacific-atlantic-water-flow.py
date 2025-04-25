b9a09ed7c2b1a5f0c9160429e8fa01b7b4eec21f
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-04-25 17:42:18 +0600

    Time: 43 ms (33.20%) | Memory: 19.3 MB (64.44%) - LeetSync

---
+++ Problems/417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -0,0 +1,35 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        n, m = len(heights), len(heights[0])
+        dp = [[0]*m for _ in range(n)]
+
+        def dfs(x: int, y: int, val: int, ck: int) -> None:
+            if x < 0 or y < 0 or x >= n or y >= m:
+                return
+            if dp[x][y] >= ck + 1 or heights[x][y] < val:
+                return
+            if dp[x][y] == ck:
+                dp[x][y] = ck + 1
+            else:
+                dp[x][y] = 3
+            for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
+                dfs(x + dx, y + dy, heights[x][y], ck)
+
+        # Pacific
+        for i in range(n):
+            dfs(i, 0, -1, 0)
+        for j in range(m):
+            dfs(0, j, -1, 0)
+        print(dp)
+        # Atlantic
+        for i in range(n):
+            dfs(i, m - 1, -1, 1)
+        for j in range(m):
+            dfs(n - 1, j, -1, 1)
+        print(dp)
+        ans = []
+        for i in range(n):
+            for j in range(m):
+                if dp[i][j] == 2:
+                    ans.append([i, j])
+        return ans
