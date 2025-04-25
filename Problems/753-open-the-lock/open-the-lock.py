03f2153a55cd711e7a8e46372b2eeaea2c427cb2
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-04-26 00:24:02 +0600

    Time: 972 ms (13.00%) | Memory: 19.1 MB (84.97%) - LeetSync

---
+++ Problems/753-open-the-lock/open-the-lock.py
@@ -0,0 +1,24 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if '0000' in deadends:
+            return -1
+        queue = deque()
+        queue.append(('0000',0))
+        visited = set('0000')
+        while queue:
+            curStr, curSteps = queue.popleft()
+            
+            if curStr == target:
+                return curSteps
+            
+            for i in range(4):
+                digit = int(curStr[i])
+                for cng in [1,-1]:
+                    newDigit = (digit+cng)%10
+                    newStr = curStr[:i] + str(newDigit) + curStr[i+1:]
+                    if newStr not in visited and newStr not in deadends:
+                        visited.add(newStr)
+                        queue.append((newStr,curSteps+1))
+        return -1
+
+            
