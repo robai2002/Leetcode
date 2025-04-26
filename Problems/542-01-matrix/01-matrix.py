3e45b204de0c2e18b5911d89daa538c38c486265
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-04-27 01:23:49 +0600

    Time: 190 ms (34.01%) | Memory: 21.6 MB (23.22%) - LeetSync

---
+++ Problems/542-01-matrix/01-matrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        seen = set()
+        ans = [[0]*len(mat[0]) for i in range(len(mat))]
+        nig = [(0,1),(0,-1),(1,0),(-1,0)]
+        # time O(n*m)
+        #space O(n*m)
+        queue = deque()
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if not mat[i][j]:
+                    queue.append((i,j))
+                    seen.add((i,j))
+        step = 0
+        while queue:
+            step += 1
+            for i in range(len(queue)):
+                x,y = queue.popleft()
+                for a,b in nig:
+                    a+=x
+                    b+=y
+                    if -1<a<len(mat) and -1<b<len(mat[0]):
+                        if (a,b) not in seen:
+                            seen.add((a,b))
+                            queue.append((a,b))
+                            ans[a][b] = step
+        return ans
