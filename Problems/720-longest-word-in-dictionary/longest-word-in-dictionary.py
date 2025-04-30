2386fcd64cbcdc6e6666949b96a1acf786080d3d
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-04-30 12:11:40 +0600

    Time: 8 ms (77.69%) | Memory: 18.3 MB (67.04%) - LeetSync

--- Problems/720-longest-word-in-dictionary/longest-word-in-dictionary.py
+++ Problems/720-longest-word-in-dictionary/longest-word-in-dictionary.py
@@ -1,41 +1,13 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.endofWord = False
-
-
-class Trie:
-
-    def __init__(self):
-        self.root = TrieNode()
-        self.root.endofWord = True
-
-    def insert(self, word: str) -> None:
-        curr = self.root
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        curr.endofWord = True
-
-    def get_max(self, node=None, w='1'):
-        if node is None:
-            node = self.root
-        ans = ""
-        if not node.endofWord:
-            return ""
-        for c in node.children:
-            s = self.get_max(node.children[c], c)
-            if len(s) > len(ans):
-                ans = s
-            elif len(s) == len(ans):
-                ans = min(ans, s)
-        return w + ans
-
-
 class Solution:
-    def longestWord(self, words: list[str]) -> str:
-        x = Trie()
+    def longestWord(self, words: List[str]) -> str:
+        visited = set()
+        visited.add("")
+        words.sort()
+        res = ""
         for word in words:
-            x.insert(word)
-        return x.get_max()[1:]
+            if word[:-1] in visited:
+                visited.add(word)
+                if len(res) < len(word):
+                    res = word
+
+        return res
