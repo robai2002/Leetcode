41d93d921a39b7623015e4e112fda66cfe652fb0
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-05-02 03:54:45 +0600

    Time: 11 ms (72.56%) | Memory: 20.9 MB (48.71%) - LeetSync

--- Problems/1397-search-suggestions-system/search-suggestions-system.py
+++ Problems/1397-search-suggestions-system/search-suggestions-system.py
@@ -1,8 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.arr = []
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def make_list(self,word):
+        cur = self.root
+        for c in word:
+            cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        
+    def insert(self,word):
+        cur =self.root
+        for c in word:
+            if c in cur.children:
+                if len(cur.arr)<3:
+                    cur.arr.append(word)
+            else:
+                return 
+            cur = cur.children[c]
+        return 
+    
+    def get_ans(self,word):
+        cur = self.root
+        ans = []
+        for c in word:
+            ans.append(cur.arr)
+            cur = cur.children[c]
+        return ans
+
 class Solution:
-    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        list_ = []
-        products.sort()
-        for i, c in enumerate(searchWord):
-            products = [ p for p in products if len(p) > i and p[i] == c ]
-            list_.append(products[:3])
-        return list_
+    def suggestedProducts(self, products: List[str], word: str) -> List[List[str]]:
+        trie = Trie()
+        trie.make_list(word)
+        for p in sorted(products):
+            trie.insert(p)
+        return trie.get_ans(word)
