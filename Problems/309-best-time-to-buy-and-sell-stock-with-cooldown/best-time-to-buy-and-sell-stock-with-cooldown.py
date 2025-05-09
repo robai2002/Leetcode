a3ce9069965df85f55d15301d8a635d616c93365
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-05-10 01:24:35 +0600

    Time: 1 ms (83.40%) | Memory: 18 MB (92.23%) - LeetSync

--- Problems/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ Problems/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,25 +1,10 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit,prvpof = 0,0
-        profit_with_buy = -2000
-        buy = 2000
-        for v in prices:
-            x = prvpof
-            prvpof,profit = max(profit,prvpof),max(profit,profit_with_buy+v)
-            if profit_with_buy + v<x:
-                profit_with_buy = x-v
-                buy = v
+    def maxProfit(self, prices: list[int]) -> int:        
 
-            profit_with_buy += max(0,buy - v)
-            buy = min(v,buy)
-            print(v,profit,prvpof,profit_with_buy,buy)
-        return profit
+        nostock, stock, cool = 0,-prices[0],0              
+                                                       
+        for p in prices[1:]:                            
 
-
-
-        
-
-
-            
-
-            
+            nostock, stock, cool = max(nostock,cool),   max(stock,nostock-p),  stock+p         
+ 
+        return max(nostock, stock, cool)                                             
