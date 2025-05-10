5471450d67c5b6c40fbb7a17b768883ba3e0c44a
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-05-10 14:33:07 +0600

    Time: 38 ms (99.28%) | Memory: 41.7 MB (13.48%) - LeetSync

--- Problems/164-maximum-gap/maximum-gap.py
+++ Problems/164-maximum-gap/maximum-gap.py
@@ -1,10 +1,5 @@
+import numpy as np
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        nums.sort()
-        maxs=0
-        for i in range(len(nums)-1):
-            tot=nums[i+1]-nums[i]
-            if(maxs<tot):
-                maxs=tot;
-        return maxs       
-        
+        if len(nums) < 2: return 0
+        return int(np.max(np.diff(np.sort(nums))))
