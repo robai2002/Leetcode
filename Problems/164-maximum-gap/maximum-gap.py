c4eb749af72e1ceba41b0308b01c97aa0c509cd2
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-05-10 14:32:30 +0600

    Time: 123 ms (93.39%) | Memory: 28.8 MB (93.70%) - LeetSync

--- Problems/164-maximum-gap/maximum-gap.py
+++ Problems/164-maximum-gap/maximum-gap.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        prv = 10**9
-        s = set(nums)
-        ans = 0
-        for v in sorted(s):
-            ans = max(ans,v-prv)
-            print(ans,v,prv)
-            prv = v
-        return ans
+        nums.sort()
+        maxs=0
+        for i in range(len(nums)-1):
+            tot=nums[i+1]-nums[i]
+            if(maxs<tot):
+                maxs=tot;
+        return maxs       
+        
