43888cb486c471ceb1dfe3e92780fb4fe274422d
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-05-12 00:10:11 +0600

    Time: 858 ms (87.39%) | Memory: 21.8 MB (92.31%) - LeetSync

---
+++ Problems/1122-longest-duplicate-substring/longest-duplicate-substring.py
@@ -0,0 +1,38 @@
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+
+        def robin_karp(v):
+            mod = 10**9+7
+            base = 256
+            h = pow(base,v-1,mod)
+            m  = defaultdict()
+            window = 0
+            for i,c in enumerate(s):
+                if i >= v:
+                    window = ((window-ord(s[i-v])*h)*base + ord(c))%mod
+                else:
+                    window = (window*base + ord(c))%mod
+
+                if window in m and s[i-v+1:i+1] == s[m[window]:m[window]+v]:
+                    return s[i-v+1:i+1]
+
+                if i>=v-1:
+                    m[window] = i-v+1
+            return "bakacoda"
+
+
+
+
+
+        l,h = 0,len(s)
+        ans = ""
+        while l<h:
+            mid = (l+h+1)//2
+            res = robin_karp(mid)
+            if res == "bakacoda":
+                h = mid - 1
+            else:
+                ans = res
+                l = mid
+    #    // return robin_karp(3)   
+        return ans
