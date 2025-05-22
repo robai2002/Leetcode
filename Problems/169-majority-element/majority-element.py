c25ea0646e3e917be49ec2efd2feec8af836599b
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-05-22 20:37:57 +0600

    Time: 7 ms (57.50%) | Memory: 19.4 MB (19.66%) - LeetSync

--- Problems/169-majority-element/majority-element.py
+++ Problems/169-majority-element/majority-element.py
@@ -1,4 +1,13 @@
+#boyer and moore majority vote Algorithm
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()#O(nlog(n))
-        return nums[len(nums)//2]
+        #moore voting algorithm 
+        x,cnt=0,0
+        for v in nums:
+            if cnt == 0:
+                x = v
+            if v==x:
+                cnt += 1
+            else:
+                cnt -= 1
+        return x
