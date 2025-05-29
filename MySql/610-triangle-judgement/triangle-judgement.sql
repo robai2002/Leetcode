80054542d5b5c7a72df23ab3ac2d80557b936ecb
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-05-29 20:34:03 +0600

    Time: 358 ms (9.03%) | Memory: 0B (100.00%) - LeetSync

--- MySql/610-triangle-judgement/triangle-judgement.sql
+++ MySql/610-triangle-judgement/triangle-judgement.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT *,
-      IF(x+y>z and y+z>x and x+z>y,"Yes","No") AS triangle
+      IF(x+y+z>GREATEST(x,y,z)*2,"Yes","No") AS triangle
 FROM Triangle
