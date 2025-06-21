25b406e72d4b8d708e8dd7a776b790b065efb62b
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-06-21 11:54:53 +0600

    Time: 7 ms (90.36%) | Memory: 20.3 MB (54.66%) - LeetSync

--- Problems/23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ Problems/23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,38 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        n = len(lists)
-
-        def marge(x,y):
-            head = ListNode(0)
-            cur = head
-            while x and y:
-                if x.val<y.val:
-                    cur.next = x
-                    x = x.next
-                else:
-                    cur.next = y
-                    y = y.next
-                cur =cur .next
-            if x:
-                cur.next = x
-            if y:
-                cur.next = y
-            return head.next
-
-
-
-        def marge_sort(low: int,high: int):
-            if low>high:
+        
+        def marge_list(l: int,h: int) ->Optional[ListNode]:
+            if l > h:
                 return None
-            if low == high:
-                return lists[low]
-            mid = (high+low)//2
-            x = marge_sort(low,mid)
-            y = marge_sort(mid+1,high)
-            return marge(x,y)
+            if l ==h:
+                return lists[h]
+            mid = (l+h)//2
+            l1,l2 = marge_list(l,mid),marge_list(mid+1,h)
+            ans = ListNode()
+            curr = ans 
+            while l1 and l2:
+                if l1.val<l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
+                curr = curr.next
+             
+            if l1:
+                curr.next = l1
+            else:
+                curr.next = l2
+            return ans.next
 
-        return marge_sort(0,n-1)
-            
+        
+        return marge_list(0,len(lists)-1)
