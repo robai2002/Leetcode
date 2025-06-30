758f3380fbf34ccb36940fca87f7495fd24f0d74
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-06-30 18:23:22 +0600

    Time: 83 ms (68.34%) | Memory: 18.4 MB (26.17%) - LeetSync

--- Problems/301-remove-invalid-parentheses/remove-invalid-parentheses.py
+++ Problems/301-remove-invalid-parentheses/remove-invalid-parentheses.py
@@ -1,48 +1,44 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        #Calculate the number of unmatched left and right 
+        def is_valid(string):
+            count = 0
+            for ch in string:
+                if ch == '(':
+                    count += 1
+                elif ch == ')':
+                    count -= 1
+                    if count < 0:
+                        return False
+            return count == 0
 
-        redundant_open=0
-        redundat_close=0
-        
-        for i in s:
-            if i=="(":
-                redundant_open+=1
-            elif i==")":
-                if redundant_open>0:
-                    redundant_open-=1
-                else:
-				        # If we don't have a matching left, then this is a misplaced right, record it.
+        visited = set()
+        queue = deque()
+        res = []
 
-                    redundat_close+=1
-        
-        ans=set()
-        
-        def dfs(index,left,right,ope,close,valid):
-		    # If we reached the end of the string, just check if the resulting expression is
-            # valid or not and also if we have removed the total number of left and right
-            # parentheses that we should have removed.
-            if index==len(s):
-                if left==right and ope==0 and close==0:
-                    ans.add(valid)
-                return 
-            
-            if s[index]=='(':
-                if ope>0:
-                    dfs(index+1,left,right,ope-1,close,valid)
-                    
-                dfs(index+1,left+1,right,ope,close,valid+"(")
-                
-            elif s[index]==')':
-                if close>0:
-                    dfs(index+1,left,right,ope,close-1,valid)
-                if  right<left:  
-                    dfs(index+1,left,right+1,ope,close,valid+")")
-            else:
-                dfs(index+1,left,right,ope,close,valid+s[index])
-                
-                    
-                
-        dfs(0,0,0,redundant_open,redundat_close,"")
-        return list(ans)
-     
+        queue.append(s)
+        visited.add(s)
+        found = False
+
+        while queue:
+            curr = queue.popleft()
+
+            if is_valid(curr):
+                res.append(curr)
+                found = True 
+
+            if found:
+                continue
+
+            for i in range(len(curr)):
+                if curr[i] not in ('(', ')'):
+                    continue 
+
+                next_str = curr[:i] + curr[i+1:]
+                if next_str not in visited:
+                    visited.add(next_str)
+                    queue.append(next_str)
+
+        return res if res else [""]
