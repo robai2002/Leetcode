c101bcc7d945cb40255bb69cccb62d49decfb6a1
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-07-04 11:37:56 +0600

    Time: 0 ms (100.00%) | Memory: 17.8 MB (81.16%) - LeetSync

--- Problems/3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.py
+++ Problems/3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.py
@@ -2,7 +2,8 @@ class Solution:
     def kthCharacter(self, k: int, operations: List[int]) -> str:
         k -= 1
         c = 0
-        for i in reversed(range(53)):
+        n = k.bit_length()
+        for i in reversed(range(n+1)):
             x = 1<<i
             if k&x:
                 c += operations[i]
