75be49665b507580843c0656a5b3f44948821e1a
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-07-05 15:43:09 +0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (9.60%) - LeetSync

--- Problems/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ Problems/1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -1,7 +1,20 @@
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        ans = -1
-        for x,y in Counter(arr).items():
-            if x==y:
-                ans = max(x,ans)
-        return ans
+        store = {}
+        luckyNumbers = []
+        
+        for x in arr:
+            if x not in store:
+                store[x] = 1
+            else:
+                store[x] = store[x] + 1
+        
+        for key,value in store.items():
+            if key == value:
+                luckyNumbers.append(key)
+        
+    
+        if not luckyNumbers:
+            return -1
+        else:
+            return max(luckyNumbers)
