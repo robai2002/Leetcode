c60f75ed3cc0180ecf5205f2ae39ab475191e7ab
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-07-11 18:12:33 +0600

    Time: 195 ms (95.19%) | Memory: 51.5 MB (35.96%) - LeetSync

--- Problems/741-cherry-pickup/cherry-pickup.py
+++ Problems/741-cherry-pickup/cherry-pickup.py
@@ -1,37 +1,16 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        dp = [[-2 for i in range(n*n)] for j in range(n*n)]
-
-        def solve(x: int,y: int,a: int,b: int) ->int:
-            nonlocal n
-            #invalid step
-            if x<0 or x>=n or y<0 or y>=n or a<0 or a>=n or b<0 or b>=n or grid[a][b] == -1 or grid[x][y] == -1:
-               # print(x,y,a,b)
-                return -1
-            if dp[a*n+b][x*n+y]>-2:
-                return dp[a*n+b][x*n+y]
-            if a==x and b==y:
-                dp[a*n+b][x*n+y] = grid[x][y]
-                if a==n-1 and a==b:
-                    return dp[a*n+b][x*n+y]
-            else:
-                dp[a*n+b][x*n+y] =grid[x][y] + grid[a][b]
-            
-            ans = max(
-                solve(x+1,y,a+1,b),solve(x+1,y,a,b+1),solve(x,y+1,a+1,b),solve(x,y+1,a,b+1)
-            )
-            if ans == -1:
-                dp[a*n+b][x*n+y]  = -1
-            else:
-                dp[a*n+b][x*n+y] += ans
-            #print(x,y,a,b,ans,dp[a*n+b][x*n+y])
-            return dp[a*n+b][x*n+y]
-        
-        
-        x = solve(0,0,0,0)
-        
-        return x if x>=0 else 0
-             
-
-            
+        N = len(grid)
+        @cache
+        def solve(r1, c1, r2, c2):
+            if not (0<=r1<N and 0<=c1<N and 0<=r2<N and 0<=c2<N): return -inf
+            if not (grid[r1][c1]!=-1 and grid[r2][c2]!=-1): return -inf
+            if r1>r2: return solve(r2, c2, r1, c1) # speed up
+            if r1==c1==r2==c2==N-1: return grid[N-1][N-1]
+            cp = grid[r1][c1] + (grid[r2][c2] if r1!=r2 or c1!=c2 else 0)
+            f1 = solve(r1+1, c1, r2+1, c2)
+            f2 = solve(r1+1, c1, r2, c2+1)
+            f3 = solve(r1, c1+1, r2+1, c2)
+            f4 = solve(r1, c1+1, r2, c2+1)
+            return cp + max(f1, f2, f3, f4)
+        return max(solve(0, 0, 0, 0), 0)
