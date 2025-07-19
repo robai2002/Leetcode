01176029da76407196ab4d00b7c2f6e6bd70d1f6
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-07-19 22:31:30 +0600

    Time: 23 ms (85.90%) | Memory: 30.5 MB (77.01%) - LeetSync

--- Problems/1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ Problems/1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,44 +1,10 @@
-class TrieNode:
-    def __init__(self, name):
-        self.name = name
-        self.children = {}
-        self.endofFolder = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode("/")
-        self.ans = []
-        self.road = []
-
-    def insert(self, path):
-        curr = self.root
-        for c in path:
-            if c not in curr.children:
-                curr.children[c] = TrieNode(c)
-            curr = curr.children[c]
-        curr.endofFolder = True
-
-    def get_ans(self, node):
-        if node != self.root:
-            self.road.append(node.name)
-
-        if node.endofFolder:
-            self.ans.append("/" + "/".join(self.road))
-            if node != self.root:
-                self.road.pop()
-            return
-
-        for child in node.children.values():
-            self.get_ans(child)
-
-        if node != self.root:
-            self.road.pop()
+from typing import List
 
 class Solution:
-    def removeSubfolders(self, folder):
-        R = Trie()
-        for s in folder:
-            s = s.split("/")[1:]  
-            R.insert(s)
-        R.get_ans(R.root)
-        return R.ans
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        res = []
+        for path in folder:
+            if not res or not path.startswith(res[-1] + '/'):
+                res.append(path)
+        return res
