938d1fa6705ae9ab6a55753bf6dfb1e24c0d14b3
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-08-29 00:17:31 +0600

    Time: 4 ms (97.08%) | Memory: 18 MB (29.58%) - LeetSync

--- Problems/3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
+++ Problems/3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.py
@@ -1,41 +1,39 @@
-from typing import List
-
 class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
-        m = len(grid[0])
 
-       
-        def _heapify(x: int, y: int, l: int, i: int,t):
-            left, right, largest = 2 * i + 1, 2 * i + 2, i
+        for k in range(n):
+            arr = []
+            i, j = k, 0
 
-            if left < l and (grid[x + left][y + left] > grid[x + largest][y + largest])==t:
-                largest = left
-            if right < l and (grid[x + right][y + right] > grid[x + largest][y + largest])==t:
-                largest = right
+            while i < n:
+                arr.append(grid[i][j])
+                i += 1
+                j += 1
+            
+            arr.sort(reverse=True)
+            i, j = k, 0
 
-            if largest != i:
-                grid[x + i][y + i], grid[x + largest][y + largest] = grid[x + largest][y + largest], grid[x + i][y + i]
-                _heapify(x, y, l, largest,t)
-
+            while i < n:
+                grid[i][j] = arr[j]
+                i += 1
+                j += 1
         
-        def heapsort(x: int, y: int,t):
-            l = min(n - x, m - y) 
+        for k in range(1, n):
+            arr = []
+            i, j = 0, k
 
-           
-            for i in reversed(range(l // 2)):
-                _heapify(x, y, l, i,t)
+            while j < n:
+                arr.append(grid[i][j])
+                i += 1
+                j += 1
+            
+            arr.sort()
+            i, j = 0, k
 
-            print(x,y,l)
-          
-            for i in reversed(range(l)):
-                grid[x][y], grid[x + i][y + i] = grid[x + i][y + i], grid[x][y]
-                _heapify(x, y, i, 0,t)
-
+            while j < n:
+                grid[i][j] = arr[i]
+                i += 1
+                j += 1
         
-        for i in range(n):
-            heapsort(i, 0,False)
-        for j in range(1, m):
-            heapsort(0, j,True)
-
         return grid
