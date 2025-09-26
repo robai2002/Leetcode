c547f23d3be372acae66fbe1e8ba720658b44402
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-09-26 15:26:09 +0600

    Time: 391 ms (98.50%) | Memory: 17.8 MB (52.50%) - LeetSync

---
+++ Problems/611-valid-triangle-number/valid-triangle-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort(reverse = True)
+        ans = 0
+        n = len(nums)
+        for ind,num in enumerate(nums):
+            l=ind +1
+            h = n-1;
+            while l<h:
+                if nums[l]+nums[h]>num:
+                    ans += h-l
+                    l+=1
+                else:
+                    h -= 1
+            
+        return ans
+
