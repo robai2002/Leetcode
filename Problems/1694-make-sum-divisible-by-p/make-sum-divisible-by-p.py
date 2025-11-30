0d0764805eddf8657fadcbd5fe64f5b6e9be0390
robai2002 <112475449+robai2002@users.noreply.github.com>
2025-11-30 12:15:53 +0600

    Time: 323 ms (5.09%) | Memory: 38.5 MB (11.11%) - LeetSync

---
+++ Problems/1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        ans = 10**7
+        d = sum(nums)
+        if d<p:return -1
+        d%=p
+        if d==0:
+            return 0
+        m = dict()
+        x = 0
+        m[0]= -1
+        for ind,num in enumerate(nums):
+            x += num
+            if x<d:
+                z = x
+            else:
+                z = (x-d)%p
+                print(ind,num,x,z,z in m)
+                if z in m:
+                    ans = min(ans,ind-m[z])
+            m[x%p]=ind
+        return ans if ans<len(nums) else -1
+        
+        
