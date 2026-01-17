051f4170b7c6496d6087296e0ee2e3201efefbbf
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-01-17 10:18:24 +0600

    Time: 2645 ms (85.71%) | Memory: 20.1 MB (5.19%) - LeetSync

--- Problems/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
+++ Problems/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
@@ -1,13 +1,15 @@
 class Solution:
-    def largestSquareArea(self, bottomLeft, topRight):
-        ans = 0
-        
-        for i, (l, r) in enumerate(zip(bottomLeft, topRight)):
-            for m, n in zip(bottomLeft[i+1:], topRight[i+1:]):
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
+        ans = 0 
+        for i,(l,r) in enumerate(zip(bottomLeft,topRight)):
+            for (m,n) in zip(bottomLeft[i+1:],topRight[i+1:]):
                 x = min(r[0], n[0]) - max(l[0], m[0])
                 y = min(r[1], n[1]) - max(l[1], m[1])
+                ans= max(ans,min(x,y))
                 
                 
-                ans = max(ans, min(x, y))
-        
-        return ans * ans
+        return ans*ans
+
+                    
+
+            
