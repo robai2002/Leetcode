3a90c37ec7fb118dc92c7c259f5bb491ea847218
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-01-17 10:17:27 +0600

    Time: 2780 ms (81.82%) | Memory: 19.7 MB (19.48%) - LeetSync

--- Problems/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
+++ Problems/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
@@ -7,7 +7,7 @@ class Solution:
                 x = min(r[0], n[0]) - max(l[0], m[0])
                 y = min(r[1], n[1]) - max(l[1], m[1])
                 
-                if x > 0 and y > 0:
-                    ans = max(ans, min(x, y))
+                
+                ans = max(ans, min(x, y))
         
         return ans * ans
