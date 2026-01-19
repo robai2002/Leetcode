883054f03e2dab864c63b04998301156e93318da
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-01-19 14:07:25 +0600

    Time: 11 ms (98.90%) | Memory: 19.7 MB (39.50%) - LeetSync

--- Problems/839-short-encoding-of-words/short-encoding-of-words.py
+++ Problems/839-short-encoding-of-words/short-encoding-of-words.py
@@ -1,12 +1,12 @@
-class Solution:
+class Solution:                         
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        words.sort(key = lambda x: -len(x))
-        ans = 0
-        for i in range(len(words)):
-            c = len(words[i])+1
-            for j in range(i):
-                if words[j][-(len(words[i])):] == words[i]:
-                    c = 0 
-                    break
-            ans+= c
+
+        words = sorted(map(lambda x: x[::-1], words))
+
+        ans = len(words[-1])+1
+
+        for i in range(len(words)-1):
+            if not words[i+1].startswith(words[i]):
+                ans+= len(words[i])+1  
+
         return ans
