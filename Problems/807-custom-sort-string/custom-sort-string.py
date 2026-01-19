426a4fe8b4ade616ac0f7fdec0789ac348c79538
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-01-19 11:37:23 +0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.24%) - LeetSync

--- Problems/807-custom-sort-string/custom-sort-string.py
+++ Problems/807-custom-sort-string/custom-sort-string.py
@@ -4,28 +4,6 @@ class Solution:
         mp = dict()
         for ind,ch in enumerate(order,start = 1):
             mp[ch] = ind
-        def heapify(ind: int,n: int) ->None:
-            largest = ind
-            if ind*2+1<n and mp.get(l[largest],0)< mp.get(l[ind*2+1],0):
-                largest = ind*2+1
-            
-            if ind*2+2<n and mp.get(l[largest],0)< mp.get(l[ind*2+2],0):
-                largest = ind*2+2
-            if ind!=largest:
-                l[ind],l[largest] = l[largest],l[ind]
-                heapify(largest,n)
-            return
-
-
-
-
-        def heap_sort():
-            for i in range(len(l) // 2 - 1, -1, -1):
-                heapify(i,len(l))
-            for i in range(len(l)-1,0,-1):
-                l[0],l[i] = l[i],l[0]
-                heapify(0,i)
-            return l
-
+        l.sort(key =lambda x: mp.get(x,0))
         
-        return "".join(heap_sort())
+        return "".join(l)
