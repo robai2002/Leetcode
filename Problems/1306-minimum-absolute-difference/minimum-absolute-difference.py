8a2cdd3196fc750ff261420a49b5f3a61b96242c
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-01-26 14:52:33 +0600

    Time: 45 ms (83.56%) | Memory: 31.5 MB (29.60%) - LeetSync

---
+++ Problems/1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        ans = 10**9
+        for x,y in pairwise(arr):
+            ans = min(ans,y-x)
+        print(ans)
+        return [[x,y] for x,y in pairwise(arr) if y-x==ans]
+        
