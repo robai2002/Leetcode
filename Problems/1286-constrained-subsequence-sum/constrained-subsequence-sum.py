75b431a022c7cf55fdb966c6bc88996e0fc5d232
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-03-01 14:00:36 +0600

    Time: 213 ms (99.69%) | Memory: 34.7 MB (91.38%) - LeetSync

--- Problems/1286-constrained-subsequence-sum/constrained-subsequence-sum.py
+++ Problems/1286-constrained-subsequence-sum/constrained-subsequence-sum.py
@@ -1,14 +1,13 @@
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-        q = []
-        heappush(q,(0,1))
-        ans = -10**6
-        for ind,num in enumerate(nums):
-            while q and k-q[0][1]<ind:
-                heappop(q)
-            z = num - (q[0][0] if q else 0)
-            num = max(z,num)
-            ans = max(ans,num)
-            heappush(q,(-num,-ind))
-        return ans
+        q = deque()
+        for i in range(len(nums)):
+            nums[i] += q[0] if q else 0
+            while q and nums[i] > q[-1]:
+                q.pop()
+            if nums[i] > 0:
+                q.append(nums[i])
+            if i >= k and q and q[0] == nums[i-k]:
+                q.popleft()
 
+        return max(nums)
