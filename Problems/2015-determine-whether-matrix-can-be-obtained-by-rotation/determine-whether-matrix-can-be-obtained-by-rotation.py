359d496f2221f26e8ca63ddb291ac26c5b4dcca5
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-03-22 10:58:24 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.42%) - LeetSync

--- Problems/2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
+++ Problems/2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -6,9 +6,9 @@ class Solution:
         n = len(mat)
         for i in range(n):
             for j in range(n):
-                if mat[i][j]!=target[i][j]: a = False
-                if mat[j][n-1-i]!= target[i][j]: b= False
-                if mat[n-1-j][i]!= target[i][j]:c = False
-                if mat[n-1-i][n-1-j] != target[i][j]:d = False
+                if mat[i][j]!=target[i][j]: a = False # 0 degree
+                if mat[n-1-j][i]!= target[i][j]: b= False # 90 degree
+                if mat[n-1-i][n-1-j]!= target[i][j]:c = False # 180 degree
+                if mat[j][n-1-i] != target[i][j]:d = False # -90 degree
         return a|b|c|d
         
