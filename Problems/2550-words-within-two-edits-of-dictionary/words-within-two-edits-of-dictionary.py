43ee98e9d2ae5f1b8f87284416ec03844991fc2d
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-04-22 06:36:14 +0600

    Time: 11 ms (78.43%) | Memory: 19.4 MB (78.43%) - LeetSync

--- Problems/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
+++ Problems/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.py
@@ -1,11 +1,20 @@
 class Solution:
-    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        n = len(queries[0])
-        f = lambda x,y: sum(x[i]!=y[i] for i in range(n))<3
-        ans = []
-        for word in queries:
-            for ck in dictionary:
-                if f(word,ck):
-                    ans.append(word)
+      def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        def matches(query, target):
+            n = len(query)
+            misses = 0
+            for i in range(n):
+                if query[i] == target[i]:
+                    continue
+                misses += 1
+                if misses > 2:
+                    return False
+            return True
+
+        res = []
+        for query in queries:
+            for word in dictionary:
+                if matches(query, word):
+                    res.append(query)
                     break
-        return ans
+        return res
