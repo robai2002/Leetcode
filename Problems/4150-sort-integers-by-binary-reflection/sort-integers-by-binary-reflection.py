782d2bc8ea2d711156b0ddc232351f11c10772fe
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-04-26 22:37:11 +0600

    Time: 11 ms (38.65%) | Memory: 19.4 MB (39.06%) - LeetSync

--- Problems/4150-sort-integers-by-binary-reflection/sort-integers-by-binary-reflection.py
+++ Problems/4150-sort-integers-by-binary-reflection/sort-integers-by-binary-reflection.py
@@ -1,6 +1,4 @@
 class Solution:
     def sortByReflection(self, nums: List[int]) -> List[int]:
-        def reflect(n):
-            if n == 0: return 0
-            return int(bin(n)[2:][::-1], 2)
-        return sorted(nums, key=lambda x: (reflect(x), x))
+        
+        return sorted(nums, key=lambda x: (int(bin(x)[2:][::-1], 2), x))
