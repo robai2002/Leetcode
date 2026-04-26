e49d3f2ea6af9a159a98acdd1301677c9437a705
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-04-26 14:59:06 +0600

    Time: 1 ms (78.01%) | Memory: 19.3 MB (88.05%) - LeetSync

--- Problems/4290-valid-elements-in-an-array/valid-elements-in-an-array.py
+++ Problems/4290-valid-elements-in-an-array/valid-elements-in-an-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def findValidElements(self, nums: list[int]) -> list[int]:
         n = len(nums)
-        A = set([0, n - 1])
-
-        l = nums[0]
-        for i in range(1, n):
-            if nums[i] > l: A.add(i)
-            l = max(l, nums[i])
-
-        r = nums[-1]
-        for i in range(n - 2, -1, -1):
-            if nums[i] > r: A.add(i)
-            r = max(r, nums[i])
-
-        return [nums[i] for i in sorted(A)]
+        dp = [0]*n
+        mx = 0
+        for ind,num in enumerate(nums):
+            if num>mx:
+                dp[ind] = num
+                mx = num
+        mx = 0
+        for ind,num in reversed(list(enumerate(nums))):
+            if num>mx:
+                dp[ind] = num 
+                mx = num
+        return [num for num in dp if num>0]
+        
