82028c03a359be5785e39264d1086be2ca8cf87b
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-05-18 00:38:30 +0600

    Time: 7 ms (97.28%) | Memory: 36.9 MB (28.76%) - LeetSync

--- Problems/1428-jump-game-iii/jump-game-iii.py
+++ Problems/1428-jump-game-iii/jump-game-iii.py
@@ -1,18 +1,11 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        n = len(arr)
-        vis = [False for i in range(n)]
-        l = [start]
-        i = 0
-        while i<len(l):
-            if arr[l[i]]==0:return True
-            x,y = l[i]+ arr[l[i]],l[i]- arr[l[i]]
-            if 0<=x<n and vis[x]==False:
-                vis[x]= True
-                l.append(x)
-            if 0<=y<n and vis[y]==False:
-                vis[y]= True
-                l.append(y)
-            i += 1
-        return False
-        
+        def dfs(i):
+            if not(0<=i<len(arr)) or arr[i]==-1:
+                return False
+            if arr[i]==0:
+                return True
+            temp=arr[i]
+            arr[i]=-1
+            return dfs(i+temp) or dfs(i-temp)
+        return dfs(start)
