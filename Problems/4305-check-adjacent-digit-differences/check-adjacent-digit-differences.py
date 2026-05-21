0eaaf32f88e5d1d8cec2bbfde548f1f7dfefbe5d
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-05-21 18:31:39 +0600

    Time: 3 ms (48.69%) | Memory: 19.3 MB (68.19%) - LeetSync

--- Problems/4305-check-adjacent-digit-differences/check-adjacent-digit-differences.py
+++ Problems/4305-check-adjacent-digit-differences/check-adjacent-digit-differences.py
@@ -1,5 +1,4 @@
 class Solution:
     def isAdjacentDiffAtMostTwo(self, s: str) -> bool:
-        l = list(int(c) for c in s)
-        return False if any(True for x,y in pairwise(l) if abs(x-y)>2) else True 
-        
+        digits = map(int, s)
+        return all(abs(a - b) <= 2 for a, b in pairwise(digits))
