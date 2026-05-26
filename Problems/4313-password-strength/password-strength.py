07a3553db122e5215f8361dd32132e857af37f67
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-05-26 10:42:29 +0600

    Time: 12 ms (57.33%) | Memory: 19.7 MB (93.48%) - LeetSync

--- Problems/4313-password-strength/password-strength.py
+++ Problems/4313-password-strength/password-strength.py
@@ -2,9 +2,9 @@ class Solution:
     def passwordStrength(self, password: str) -> int:
         ans = 0
         for ch in set(password):
-            if 'a'<=ch<='z':ans+= 1
-            elif 'A'<=ch<='Z':ans+=2
-            elif '0'<=ch<='9':ans+=3
+            if ch.islower():ans+= 1
+            elif ch.isupper():ans+=2
+            elif ch.isdigit():ans+=3
             else: ans+=5
         return ans
         
