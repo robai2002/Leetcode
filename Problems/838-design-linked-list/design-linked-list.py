8beb6096785ff20dbce28d102e1e43d55d695afd
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-06-03 14:03:35 +0600

    Time: 11 ms (98.81%) | Memory: 19.9 MB (95.39%) - LeetSync

--- Problems/838-design-linked-list/design-linked-list.py
+++ Problems/838-design-linked-list/design-linked-list.py
@@ -1,50 +1,37 @@
-class Node:
-    def __init__(self, val, next=None):
-        self.val = val
-        self.next = next
-
 class MyLinkedList:
 
     def __init__(self):
-        self.head = Node(-1)
-        self.length = 0
+        self.list = []
         
 
     def get(self, index: int) -> int:
-        if index < 0 or index >= self.length:
+        if not (0 <= index < len(self.list)):
             return -1
-        curr = self.head.next
-        for _ in range(index):
-            curr = curr.next
-        return curr.val
+
+        return self.list[index]
         
+
     def addAtHead(self, val: int) -> None:
-        self.addAtIndex(0, val)
+        self.list.insert(0, val)
         
 
     def addAtTail(self, val: int) -> None:
-        self.addAtIndex(self.length, val)
+        self.list.append(val)
+        
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index > self.length:
-            return -1
-        prev, curr = self.head, self.head.next
-        for _ in range(index):
-            prev = curr
-            curr = curr.next
-        prev.next = Node(val, curr)
-        self.length += 1
+        if index > len(self.list):
+            return
 
+        self.list.insert(index, val)
+        
 
     def deleteAtIndex(self, index: int) -> None:
-        if index >= self.length or index < 0:
-            return -1
-        prev, curr = self.head, self.head.next
-        for _ in range(index):
-            prev = curr
-            curr = curr.next
-        prev.next = curr.next
-        self.length -= 1
+        if not (0 <= index < len(self.list)):
+            return
+
+        self.list.pop(index)
+        
 
 
 # Your MyLinkedList object will be instantiated and called as such:
