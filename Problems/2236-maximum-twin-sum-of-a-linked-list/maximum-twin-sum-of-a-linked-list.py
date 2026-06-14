9a33b5a3f9c1cd5d8ca61ad001f80be33bcce03d
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-06-14 12:23:20 +0600

    Time: 53 ms (82.96%) | Memory: 50.5 MB (57.14%) - LeetSync

--- Problems/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ Problems/2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -8,18 +8,18 @@ class Solution:
 	    slow, fast = head, head
 	    maxVal = 0
 
-	# Get middle of linked list
+	
 	    while fast and fast.next:
 		    fast = fast.next.next
 		    slow = slow.next
 
-	# Reverse second part of linked list
+	
 	    curr, prev = slow, None
 
 	    while curr:       
 		    curr.next, prev, curr = prev, curr, curr.next   
 
-	# Get max sum of pairs
+	
 	    while prev:
 		    maxVal = max(maxVal, head.val + prev.val)
 		    prev = prev.next
