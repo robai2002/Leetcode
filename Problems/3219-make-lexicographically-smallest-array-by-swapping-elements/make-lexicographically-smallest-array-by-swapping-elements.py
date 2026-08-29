2d5c5b785f89ff1e72bce03e1eabf9ea50a6a3cd
robai2002 <112475449+robai2002@users.noreply.github.com>
2026-08-30 00:36:21 +0600

    Time: 247 ms (76.12%) | Memory: 46.8 MB (79.85%) - LeetSync

--- Problems/3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ Problems/3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,24 +1,28 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        temp = []
-        for ind,num in enumerate(nums):
-            temp.append((num,ind))
-        temp.sort()
-        i,j,n = 0,0,len(nums)
-        
-       
-        while i<n:
-            print(i)
-            ind = [temp[i][1]]
-            j = i + 1
-            while j<n and temp[j][0] - temp[j-1][0] <= limit:
-                ind.append(temp[j][1])
-                j += 1 
-            ind.sort(reverse = True)
-            print(ind)
-            while ind:
-                x = ind.pop()  
-                nums[x] = temp[i][0]
-                i += 1
-        return nums 
-        
+        n = len(nums)
+
+        pairs = [(nums[i], i) for i in range(n)]
+        pairs.sort()
+
+        l = 0
+
+        while l < n:
+            r = l + 1
+
+            while r < n and pairs[r][0] - pairs[r - 1][0] <= limit:
+                r += 1
+
+            group = []
+
+            for i in range(l, r):
+                group.append(pairs[i][1])
+
+            group.sort()
+
+            for i in range(len(group)):
+                nums[group[i]] = pairs[l + i][0]
+
+            l = r
+
+        return nums
